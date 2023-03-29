8e5604dbc16089f0cd3434141cc837c3a07d8149
Joshna1702 <129253304+Joshna1702@users.noreply.github.com>
2023-03-29 12:20:18 +0530

    Update Divby3.js

--- Divby3.js
+++ Divby3.js
@@ -1,2 +1,11 @@
-var a=10;
-console.log(a);
+var arr=[1,2,3,4,5,6,7,8,9,10];
+var result="";
+for(var i=0; i<10; i++)
+{
+    if(arr[i] % 3 == 0)
+    {
+        result=result+(arr[i]+" ")
+    }
+}
+console.log(result);
+
